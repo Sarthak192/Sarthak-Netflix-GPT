5f5ce02ec0f1ba04871035f0d6d0349101137eba
Sarthak192 <sarthaksaxena1997@gmail.com>
2024-03-28 12:15:57 +0530

    Add the firebase sign in and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
           .then( ( userCredential ) => {
             const user = userCredential.user;
             updateProfile( user, {
-              displayName: name.current.value, photoURL: "https://play-lh.googleusercontent.com/NUfCO2kD6ws3q0SQ3-sYdyzFzjR_sJTW3unilh0GjuMIpBIjARZQLA0HUisItVEJUZ3L=w240-h480-rw"
+              displayName: name.current.value, photoURL: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
             } ).then( () => {
               const { uid, email, displayName, photoURL } = auth.currentUser;
               dispatch( addUser( { uid, email, displayName, photoURL } ) );
@@ -71,7 +71,7 @@ const Login = () => {
       <div className="absolute">
         <img alt="Background Logo" src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
       </div>
-      <form className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80" onSubmit={(e) => e.preventDefault()}>
         <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm && (
           <input ref={name} type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700" />
