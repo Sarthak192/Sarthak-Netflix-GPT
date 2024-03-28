e91f66e85ccada2e8b77d6b78db70101b3ffba62
Sarthak192 <sarthaksaxena1997@gmail.com>
2024-03-28 13:41:26 +0530

    Add the firebase sign in and signup

--- src/slice/userSlice.js
+++ src/slice/userSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const userSlice = createSlice( {
   name: 'user',
@@ -9,9 +9,9 @@ const userSlice = createSlice( {
     },
     removeUser: ( state, action ) => {
       return null;
-    },
+    }
   }
-});
+} );
 
 export const { addUser, removeUser } = userSlice.actions;
 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGyxS5705LPtV-tEzVCwxxq_jEL5IjJqk",
-  authDomain: "netflix-gpt-sarthak.firebaseapp.com",
-  projectId: "netflix-gpt-sarthak",
-  storageBucket: "netflix-gpt-sarthak.appspot.com",
-  messagingSenderId: "764060296589",
-  appId: "1:764060296589:web:59f746b98d47e7c43489c4"
+  apiKey: 'AIzaSyAGyxS5705LPtV-tEzVCwxxq_jEL5IjJqk',
+  authDomain: 'netflix-gpt-sarthak.firebaseapp.com',
+  projectId: 'netflix-gpt-sarthak',
+  storageBucket: 'netflix-gpt-sarthak.appspot.com',
+  messagingSenderId: '764060296589',
+  appId: '1:764060296589:web:59f746b98d47e7c43489c4'
 };
 
 export const app = initializeApp( firebaseConfig );
